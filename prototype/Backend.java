56152ffafa892aaef9b93db929c7605cb8042105
Yasmin Woodlock <yasminwoodlock@gmail.com>
2025-04-08 10:24:57 +0100

    added id for account

--- prototype/Backend.java
+++ prototype/Backend.java
@@ -2,9 +2,11 @@ public class Backend {
     //account class
     record Account(int account_id, String firstName, String lastName, Boolean isHost){}
 
-    record Movie(int movie_id, String name){}
+    record Movie(int movie_id, String name, String genre, int rating, Interest interest){}
 
-    //movie event
-    //location
-    //interest
+    record MovieEvent(int event_id, Movie movie, String hostFirstName, String hostLastName, Location location, String time, String date){}
+
+    record Location(String eircode){}
+
+    record Interest(int interestID, Account account, Movie movie){}
 }
